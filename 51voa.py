c6b81b4251cb53e40914469c1eef9611f6dbc313
liuhui <sheldon.ctc@gmail.com>
2012-02-24 10:52:39 +0800

    New Feature: Now it can add the VOA original mat

--- 51voa.py
+++ 51voa.py
@@ -2,17 +2,20 @@
 
 from BeautifulSoup import BeautifulSoup
 import re
-from urllib import urlopen
+try:
+    from urllib import urlopen
+except Exception:
+    from urllib.request import urlopen
 import sqlite3
 
 db_file = 'voa.db'
 
-def ProgramList(url):
+def ProgramList(url, cur):
 
     reg_document = re.compile('<!DOCTYPE.*?>')
 
     #html_data = open('Health_Report_1.html').read()
-    html_data = urlopen(url).read()
+    html_data = urlopen(url).read().decode('utf-8')
     html_data = reg_document.sub('', html_data)
 
     soup = BeautifulSoup(html_data)
@@ -28,13 +31,14 @@ def ProgramList(url):
         # tmp['href'] = 'http://www.51voa.com' + a['href']
         title = a.string
         url =  'http://www.51voa.com' + a['href']
-        print title
-        GetProgramContent(title, url)
+        print(title)
+        GetProgramContent(title, url, cur)
         #Health_Report_List.append(tmp)
+        break
 
     return Health_Report_List
 
-def GetProgramContent(title, url):
+def GetProgramContent(title, url, cur):
     """This function use to get the content of the program,
     contain origin material text, mp3, lrc, translation.
 
@@ -46,7 +50,7 @@ def GetProgramContent(title, url):
     reg = re.compile('<.*?>')
 
     reg_document = re.compile('<!DOCTYPE.*?>')
-    html_data = urlopen(url).read()
+    html_data = urlopen(url).read().decode('utf-8')
     html_data = reg_document.sub('', html_data)
 
     soup = BeautifulSoup(html_data)
@@ -61,17 +65,17 @@ def GetProgramContent(title, url):
         lrc = 'http://www.51voa.com' + a[1]['href']
         trans = 'http://www.51voa.com/VOA_Special_English/' \
             + a[2]['href']
-        print trans
+        print(trans)
         # Save the Translation to file
-        getTrans(trans, title+'.trans')
+        #getTrans(trans, title+'.trans')
     elif len(a) == 3:
     # This Program May be just contain mp3, tran
         lrc = 'http://www.51voa.com' + a[1]['href']
-    savefile(mp3, title + '.mp3')
-    savefile(lrc, title + '.lrc')
+    #savefile(mp3, title + '.mp3')
+    #savefile(lrc, title + '.lrc')
 
-    text = purifyContent(soup)
-    savefile(text, title + '.txt', 2)
+    text = purifyContent(soup, title, cur)
+    #savefile(text, title + '.txt', 2)
 
 def getTrans(url, filename):
     """This function use to get the translation content.
@@ -83,14 +87,14 @@ def getTrans(url, filename):
 
     # Delete The Tag <!DOCTYPE ......>
     reg_document = re.compile('<!DOCTYPE.*?>')
-    data = urlopen(url).read()
+    data = urlopen(url).read().decode('utf-8')
     data = reg_document.sub('', data)
 
     soup = BeautifulSoup(data)
     text = purifyContent(soup)
     savefile(text, filename, 2)
 
-def purifyContent(soup):
+def purifyContent(soup, title='', cur=''):
     """This function use to purify the Program text content
     which contain some HTML sybaml.
 
@@ -103,8 +107,18 @@ def purifyContent(soup):
     div_content = soup.find('div', {'id':'content'})
     for i in div_content.findAll('div'):
         i.extract()
-    content =  reg.sub('\n', reg_strong.sub('', str(div_content)))
+    print(div_content)
+    content =  reg.sub('\n', reg_strong.sub('', str(div_content).decode('utf-8')))
     content =  re.sub('\n\n\n', '\n\n', content)
+
+    if title !='' and cur != '':
+        title = title.replace("'", "''")
+        title = title.replace('"','""')
+        content = content.replace("'", "''")
+        content = content.replace('"','""')
+
+        artical = [title, content, '', '']
+        Insert2DB(artical, 'economics', cur)
 
     return content
 
@@ -126,26 +140,29 @@ def CreateTable(db, tname):
     """
     """
     # --------- Create DataBase ----------
-    create_DB = "CREATE TABLE %s (title text, ftype varchar(3), publish date)" %(tname)
-    db.execute(create_DB)
+    create_DB = "CREATE TABLE %s (title text,content text, ftype varchar(3), publish date)" %(tname)
+    try:
+        db.execute(create_DB)
+    except sqlite3.Error:
+        pass
 
-def Insert2DB(item, tname, db):
+def Insert2DB(item, tname, cur):
     """
     """
-    insert_DB = "INSERT INTO %s VALUES('%s', '%s', '%s')" % (tname, item[0], item[1], item[2])
-    print insert_DB
-    db.execute(insert_DB)
+    print(item)
+    insert_DB = 'INSERT INTO %s VALUES("%s", "%s", "%s","%s")' % (tname, item[0], item[1], item[2], item[3])
+    # Escape "
+    #print insert_DB
+    cur.execute(insert_DB)
 
 if __name__ == '__main__':
 
     conn = sqlite3.connect(db_file)
     cur = conn.cursor()
+    CreateTable(cur, "economics")
 
     economics_report_url = 'http://www.51voa.com/Economics_Report_1.html'
-    #ProgramList(economics_report_url)
-    CreateTable(cur, "economics")
-    items = ['Websites Cut Service to Protest US Antipiracy Bills', 'mp3', '20120202']
-    Insert2DB(items, 'economics', cur)
+    ProgramList(economics_report_url, cur)
 
     conn.commit()
     cur.close()
